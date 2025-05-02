f4f89eb80b30d8b3da52690d037d411da4134518
rsdk1 <rs.dk.00001@gmail.com>
2025-05-02 17:31:09 +0530

    next prog

---
+++ src/Number.java
@@ -0,0 +1,19 @@
+public class Number
+{
+    public static void main(String[] args) {
+        int number = -80;
+
+        if (number > 0)
+        {
+            System.out.println("number is positive.");
+        }
+        else if (number < 0)
+        {
+            System.out.println("number is negative.");
+        }
+        else
+        {
+            System.out.println("number is zero.");
+        }
+    }
+}
