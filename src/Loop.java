485fd161f0861f99576be1685b44cbd81fa47e44
rsdk1 <rs.dk.00001@gmail.com>
2025-05-05 15:02:00 +0530

    next pro

---
+++ src/Loop.java
@@ -0,0 +1,9 @@
+public C:\Users\rsdk1\IdeaProjects\Testing2\src\Loop.javaclass Example2
+{
+    public static void main(String[] args )
+    {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
