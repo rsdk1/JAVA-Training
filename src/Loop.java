41b085f27f616c7eca43f2d6ab598071a80b6703
rsdk1 <rs.dk.00001@gmail.com>
2025-05-09 13:37:37 +0530

    LargeSmall
    # Conflicts:
    #	src/Loop.java

--- src/Loop.java
+++ src/Loop.java
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
 public class Example2
+=======
+ public class Loop
+>>>>>>> 53211e6 (Your commit message)
 {
     public static void main(String[] args )
     {
         for (int i = 1; i <= 20; i++)
+<<<<<<< HEAD
             {
+=======
+        {
+>>>>>>> 53211e6 (Your commit message)
             System.out.println("Number: " + i);
         }
     }
