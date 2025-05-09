d652ea945fbeab802254ae21b4d1e3f9676db904
rsdk1 <rs.dk.00001@gmail.com>
2025-05-09 16:08:46 +0530

    LargeSmall

--- src/Loop.java
+++ src/Loop.java
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-public class Example2
-=======
  public class Loop
->>>>>>> 53211e6 (Your commit message)
 {
     public static void main(String[] args )
     {
         for (int i = 1; i <= 20; i++)
-<<<<<<< HEAD
-            {
-=======
         {
->>>>>>> 53211e6 (Your commit message)
             System.out.println("Number: " + i);
         }
     }
