9ce42c462536e24ab7e70299d23e7fcf3560ddf9
rsdk1 <rs.dk.00001@gmail.com>
2025-05-11 08:04:40 +0530

    new pro

---
+++ src/Arr.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class  Arr {
+    public static void main (String[] args){
+        int marks [] = {23,45,67,89};
+        System.out.println(Arrays.toString(marks));
+    }
+}
