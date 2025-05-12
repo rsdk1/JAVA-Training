46a8207b00a99fe093a17429c78e574853ffcb6d
rsdk1 <rs.dk.00001@gmail.com>
2025-05-12 20:12:07 +0530

    date pro

---
+++ src/Date.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Date {
+    public static void main (String[] args)
+    {
+        String a = "GitHub";
+        LocalDate today=LocalDate.now();
+        System.out.println(a);
+        System.out.println(today);
+    }
+}
